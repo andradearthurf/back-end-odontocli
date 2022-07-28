f9dd43356685199cad82a292fd4e0578e25ba371
Arthur <andradearthurf@gmail.com>
2022-07-28 20:57:52 -0300

    validation updates with yup and new get route created

--- src/routes/pacientes.routes.ts
+++ src/routes/pacientes.routes.ts
@@ -17,18 +17,22 @@ pacientesRouter.post('/create', async (request, response) => {
     cepEndereco,
     cidade } = request.body;
 
-    const dateRegex = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
+  const dateRegex = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
+    
+  const nameRegex = /^[A-ZÀ-Ÿ][A-zÀ-ÿ']+\s([A-zÀ-ÿ']\s?)*[A-ZÀ-Ÿ][A-zÀ-ÿ']+$/;
 
-    let schema = yup.object().shape({
-      cpf: yup.string().required("O cpf é obrigatório!").max(11, "A quantidade de números é onze!").min(11, "A quantidade de números é onze!").matches(/^[0-9]+$/, "O cpf aceita apenas números!"),
-      nomeCompleto: yup.string().required("O nome do paciente é obrigatório!").matches(/^[aA-zZ\s]+$/, "O nome do paciente deve conter apenas letras!"),
-      telefone: yup.string().required("O telefone é obrigatório!").matches(/^[0-9]+$/, "O telefone aceita apenas números!").max(12, "A quantidade de números do telefone é 12!").min(12, "A quantidade de números do telefone é 12!"),
-      dtNascimento: yup.string().required("A data de nascimento é obrigatória!").matches(dateRegex, "O formato deve ser dd/mm/yyyy!"),
-      numeroEndereco: yup.string().required("O número de endereço é obrigatório!").matches(/^[0-9]+$/, "O número de endereço aceita apenas números!"),
-      logradouroEndereco: yup.string().required("O logradouro é obrigatório!"),
-    });
+  let schema = yup.object().shape({
+    cpf: yup.string().required("O cpf é obrigatório!").max(11, "A quantidade de números é onze!").min(11, "A quantidade de números é onze!").matches(/^[0-9]+$/, "O cpf aceita apenas números!"),
+    nomeCompleto: yup.string().required("O nome do paciente é obrigatório!").matches(nameRegex, "O nome do paciente deve estar no formato certo!"),
+    telefone: yup.string().required("O telefone é obrigatório!").matches(/^[0-9]+$/, "O telefone aceita apenas números!").max(12, "A quantidade de números do telefone é 12!").min(12, "A quantidade de números do telefone é 12!"),
+    dtNascimento: yup.string().required("A data de nascimento é obrigatória!").matches(dateRegex, "O formato deve ser dd/mm/yyyy!"),
+    numeroEndereco: yup.string().required("O número de endereço é obrigatório!").matches(/^[0-9]+$/, "O número de endereço aceita apenas números!"),
+    logradouroEndereco: yup.string().required("O logradouro é obrigatório!"),
+    cepEndereco: yup.string().required("O cep é obrigatório!").matches(/^([0-9]{5})\-([0-9]{3})$/, "O formato do cep deve ser nnnnn-nnn!"),
+    cidade: yup.string().required("A cidade é obrigatória!"),
+  });
 
-    await schema.validate(request.body);
+  await schema.validate(request.body);
 
   const createPaciente = new CreatePaciente();
 
@@ -54,5 +58,15 @@ pacientesRouter.get('/list', async (request, response) => {
   return response.json(pacientes);
 
 });
+
+pacientesRouter.get('/count', async (request, response) => {
+  const pacienteRepository = getCustomRepository(PacienteRepository);
+
+  const qtdPacientes = (await pacienteRepository.find()).length;
+
+  return response.json(qtdPacientes);
+
+});
+
 
 export default pacientesRouter;
