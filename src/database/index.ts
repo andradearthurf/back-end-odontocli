57b03fd5ff8c5b0405e6daa91ea91448b055dcdf
Arthur <andradearthurf@gmail.com>
2022-08-07 15:22:34 -0300

    setup docker to run backend with docker compose

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,7 +5,7 @@ export default async (host = "db_odontocli"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      host: host,
       database:
         process.env.NODE_ENV === "test"
           ? "odontocli_test"
